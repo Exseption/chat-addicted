de52c36cbeb7a6d4664337ea81cba8266fbb3b0a
Brainsurgery <brain5ur9ery@gmail.com>
2017-04-18 12:07:34 +0900

    фиксики

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-app.listen(8000);
+// var redis = require('redis'),
+// client = redis.createClient();
+// client.select(0, function () {
+//     client.on('error', function (err) {
+//         console.log('Error ' + err);
+//     });
+//     client.quit();
+// });
 
+
+app.listen(8000);
 function handler (req, res) {
     fs.readFile(__dirname + '/index.html',
         function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
             res.writeHead(200);
             res.end(data);
         });
@@ -19,17 +24,64 @@ var another = io.of('/another');
 another.on('connection', function (socket) {
     console.log('Кто-то подключился к другому каналу!');
     socket.send('Пдключился к другой комнате!')
-})
+});
+
+var users = [];
 
 io.on('connection', function (socket) {
-    console.log('Кто-то подключился к основному каналу!')
-    socket.on('message', function (data) {
-        console.log(data);
-    });
-    socket.on('mess', function (data) {
-        console.log("Сообщение от "+ data.nick + " " + data.data);
-        socket.json.send({nick: 'Вы', data: data.data});
-        socket.broadcast.emit('s:mess', {data: data.data, nick: data.nick})
 
+    console.log('Кто-то подключился к основному каналу! ' + socket.id);
+
+    socket.on('hello', function (data) {
+
+        socket.name = data.nick;
+
+        if(users.indexOf(socket.name) > -1) {
+            socket.emit('hello:error', {data: 'Такой ник уже занят'})
+        } else {
+            console.log('Подцепился юзер с ником ' + socket.name);
+
+            users.push(data.nick);
+            socket.json.emit('server:hello', {users: users});
+            socket.broadcast.emit('server:hello', {users: users})
+        }
+
+
+    });
+
+    socket.on('mess', function (data) {
+        console.log('Обычное сообщение!');
+
+
+
+        if((data.data.substring(data.data.length - 3) === 'png') ||
+            (data.data.substring(data.data.length - 3) === 'bmp') ||
+            (data.data.substring(data.data.length - 3) === 'jpg') ||
+            (data.data.substring(data.data.length - 3) === 'gif')) {
+            console.log('Опа, картинка!');
+            var img = data.data;
+            data.data = "<img src='" + img + "'/></img>";
+        } else if(data.data.indexOf('http') === 0){
+            console.log('Ссылка!');
+            var link = data.data;
+            data.data = "<a href='" + link + "'/>" + link +"</a>";
+        }
+
+
+
+
+        socket.json.send({ nick: 'Вы', data: data.data });
+        socket.broadcast.emit('s:mess', {data: data.data, nick: data.nick})
+    }
+);
+    
+    socket.on('disconnect', function () {
+        console.log('Отвалился '+ socket.name);
+        var index = users.indexOf(socket.name);
+        if(index > -1){
+            users.splice(index,1);
+            socket.json.emit('server:hello', {users: users});
+            socket.broadcast.emit('server:hello', {users: users})
+        }
     })
 });
